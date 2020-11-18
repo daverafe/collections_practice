724c1353da282b41dc432cbb2ad3b2ec789b3518
David Rafe <david.rafe@gmail.com>
2020-11-17 21:17:46 -0500

    commit tree 1c13b4cd80279d60eafa0ff968d829c789e0d422

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+require "pry"
 
+def sort_array_asc(array)
+    array.sort 
+end
+
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array 
+end
+
+def reverse_array(array)
+    array.reverse 
+end
+
+def kesha_maker(array)
+    kesha = []
+    array.each do |str|
+     str[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |str|
+        str[0] == "a"
+    end
+end
+
+def sum_array(array)
+    array.inject do |sum, num|
+        sum + num
+    end
+end
+
+def add_s(array)
+    array.each_with_index.collect do |element, index| 
+        if element != "feet"
+            element + "s"
+        else
+            "feet"
+        end
+    end
+end
